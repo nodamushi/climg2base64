859f9c973680bb57dd46ca470ffdd39fd2e16e41
nodamushi <nodamushi@outlook.jp>
2024-12-02 02:41:08 +0900

    update cross build script

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 repository=$(dirname "$0")
 repository=$(readlink -f "$repository")
 
-mkdir -p "$repository/output"
-
+mkdir -p "$repository/output/.tmp"
+echo "*" > "$repository/output/.gitignore"
 podman run --rm -it \
+  -v "$repository/container/build.sh:/build.sh:ro" \
   -v "$repository:/climg2base64:ro" \
   -v "$repository/output:/output" \
-  climg2base64_build /climg2base64 /output
+  -v "$repository/output/.tmp:/build_dir" \
+  climg2base64_build /climg2base64 /output /build_dir
--- container/build.sh
+++ container/build.sh
@@ -1,65 +1,81 @@
 #!/bin/bash -eu
+# * mount this file to /build.sh
 workspace=$1
 output=$2
+build_dir=$3
 arch=$(uname -m)
 
 cd "$workspace"
 
+FILE=1
+NOFILE=0
+
+LINUX=0
+WIN=1
+
+build() {
+  os=$1
+  flag=$2
+  build_target=$3
+  file=$4
+  save_dirname=$5
+  cd "$workspace"
+
+  if [ $os -eq $WIN ];then
+    exec=climg2base64.exe
+    pack=climg2base64-${save_dirname}.zip
+    pack_cmd=zip
+    pack_arg=($pack $exec)
+  else
+    exec=climg2base64
+    pack=climg2base64-${save_dirname}.tar.gz
+    pack_cmd=tar
+    pack_arg=(-zcf $pack $exec)
+  fi
+
+  args=(build --release --target "$build_target")
+
+  if [ "$file" -eq $FILE ];then
+    args+=(--features file)
+    if [ $os -eq $WIN ];then
+      tmpdir=${build_dir}/${build_target}
+      outdir=$output/${save_dirname}
+    else
+      tmpdir=${build_dir}/${build_target}/no_file
+      outdir=$output/${save_dirname}/no_file
+    fi
+  else
+    tmpdir=${build_dir}/${build_target}/file
+    outdir=$output/${save_dirname}/file
+  fi
+
+  envvar=("CARGO_TARGET_DIR=$tmpdir" "CARGO_HOME=${build_dir}/.cargo")
+  if [ -n "$flag" ];then
+    envvar+=("RUSTFLAGS=-C $flag")
+  fi
+
+  env "${envvar[@]}" cargo "${args[@]}"
+
+  mkdir -p $outdir
+  cp $tmpdir/${build_target}/release/$exec $outdir
+  cd $outdir
+  $pack_cmd "${pack_arg[@]}"
+  md5sum $pack > md5.txt
+}
+
 #  x86_64-unknown-linux-gnu
-RUSTFLAGS="-C linker=x86_64-linux-gnu-gcc" \
-CARGO_TARGET_DIR=/tmp/linux-x86_64/target_no_file \
-  cargo build --release --target x86_64-unknown-linux-gnu
-
-mkdir -p "$output/linux-x86_64/no_file"
-cp /tmp/linux-x86_64/target_no_file/x86_64-unknown-linux-gnu/release/climg2base64 "$output/linux-x86_64/no_file"
-cd "$output/linux-x86_64/no_file"
-tar -zcf climg2base64-linux-x86_64.tar.gz ./climg2base64
-
+build $LINUX "linker=x86_64-linux-gnu-gcc" x86_64-unknown-linux-gnu $NOFILE linux-x86_64
 if [ "$arch" = "x86_64" ];then
   # NOTE: --feature file can not cross compile
-  cd "$workspace"
-  RUSTFLAGS="-C linker=x86_64-linux-gnu-gcc" \
-  CARGO_TARGET_DIR=/tmp/linux-x86_64/target_file \
-    cargo build --release --target x86_64-unknown-linux-gnu --features file
-
-  mkdir -p "$output/linux-x86_64/file"
-  cp /tmp/linux-x86_64/target_file/x86_64-unknown-linux-gnu/release/climg2base64 "$output/linux-x86_64/file"
-  cd "$output/linux-x86_64/file"
-  tar -zcf climg2base64-linux-x86_64.tar.gz ./climg2base64
+  build $LINUX "linker=x86_64-linux-gnu-gcc" x86_64-unknown-linux-gnu $FILE linux-x86_64
 fi
 
 # aarch64-unknown-linux-gnu
-cd "$workspace"
-RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc" \
-CARGO_TARGET_DIR=/tmp/linux-aarch64/target_no_file \
-  cargo build --release --target aarch64-unknown-linux-gnu
-
-mkdir -p "$output/linux-aarch64/no_file"
-cp /tmp/linux-aarch64/target_no_file/aarch64-unknown-linux-gnu/release/climg2base64 "$output/linux-aarch64/no_file"
-cd "$output/linux-aarch64/no_file"
-tar -zcf climg2base64-linux-aarch64.tar.gz ./climg2base64
-
-
+build $LINUX "linker=aarch64-linux-gnu-gcc" aarch64-unknown-linux-gnu $NOFILE linux-aarch64
 if [ "$arch" = "aarch64" ];then
   # NOTE: --feature file can not cross compile
-  cd "$workspace"
-  RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc" \
-  CARGO_TARGET_DIR=/tmp/linux-aarch64/target_file \
-    cargo build --release --target aarch64-unknown-linux-gnu --features file
-
-  mkdir -p "$output/linux-aarch64/file"
-  cp /tmp/linux-aarch64/target_file/aarch64-unknown-linux-gnu/release/climg2base64 "$output/linux-aarch64/file"
-  cd "$output/linux-aarch64/file"
-  tar -zcf climg2base64-linux-aarch64.tar.gz ./climg2base64
+  build $LINUX "linker=aarch64-linux-gnu-gcc" aarch64-unknown-linux-gnu $FILE linux-aarch64
 fi
 
 # windows
-cd "$workspace"
-CARGO_TARGET_DIR=/tmp/windows/target \
-    cargo build --release --features "file" \
-    --target x86_64-pc-windows-gnu
-
-mkdir -p "$output/windows-x86_64"
-cp /tmp/windows/target/x86_64-pc-windows-gnu/release/climg2base64.exe "$output/windows-x86_64"
-cd "$output/windows-x86_64"
-zip climg2base64-windows-x86_64.zip ./climg2base64.exe
+build $WIN "" x86_64-pc-windows-gnu $FILE windows-x86_64
